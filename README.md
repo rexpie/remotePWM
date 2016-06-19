6f3fc4253265ce4a5e4c270a0993bda7f19394b2
Rex Xia <rexpie@gmail.com>
2016-06-19 11:00:05 +0800

    init

---
+++ README.md
@@ -0,0 +1,18 @@
+# Control PWM output on Arduino board using your phone browser
+
+## Install
+
+    npm install
+
+
+## Run
+
+    npm start
+
+After the script starts, a new QR code will appear in the folder.
+Scan it with any QR code scanner on your phone, and if the scanner prompts for permission
+to open a browser, please give your consent.
+
+First put your phone flat on the table, then grab it up to a vertical 'selfie' posture (not the horizontal gaming posture). You should see the PWM output change by attaching an LED or motor on to pin 11.
+
+
---
+++ server.js
@@ -0,0 +1,89 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io')(app);
+var fs = require('fs');
+var five = require("johnny-five");
+var board = new five.Board();
+
+var qr = require('qr-image');
+var fs = require('fs');
+
+var address;
+var os = require('os');
+var ifaces = os.networkInterfaces();
+
+var port = 9169;
+
+
+Object.keys(ifaces).forEach(function (ifname) {
+  var alias = 0;
+
+  ifaces[ifname].forEach(function (iface) {
+    if ('IPv4' !== iface.family || iface.internal !== false) {
+      // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
+      return;
+    }
+
+    if (alias >= 1) {
+      // this single interface has multiple ipv4 addresses
+      console.log(ifname + ':' + alias, iface.address);
+    } else {
+      // this interface has only one ipv4 adress
+      console.log(ifname, iface.address);
+      address = iface.address;
+    }
+    ++alias;
+  });
+});
+
+var code = qr.image(address + ':' + port + '/mobile.html', {
+  type: 'png'
+});
+var output = fs.createWriteStream('qr.png');
+code.pipe(output);
+
+
+var led;
+board.on("ready", function () {
+  led = new five.Led(11);
+
+  // Add servo to REPL (optional)
+  this.repl.inject({
+    led: led
+  });
+
+});
+
+app.listen(port);
+
+function handler(req, res) {
+  var url = req.url;
+  if (req.url.indexOf('?') > 0) {
+    url = req.url.substr(0, req.url.indexOf('?'));
+  }
+  fs.readFile(__dirname + url,
+    function (err, data) {
+      if (err) {
+        res.writeHead(500);
+        return res.end('Error loading index.html');
+      }
+
+      res.writeHead(200);
+      res.end(data);
+    });
+}
+
+io.on('connection', function (socket) {
+  socket.on('orientation', function (data) {
+    // console.log(data);
+    if (led) {
+      led.brightness(Math.floor(Math.abs(Math.sin(data.beta / 180 * 3.1415926)) * 255));
+    }
+    // io.emit('control', data);
+  });
+
+  socket.on('motion', function (data) {
+    console.log(data);
+
+    // io.emit('control', data);
+  });
+});
